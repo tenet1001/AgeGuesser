d6597591836edaf3aac33fd1d13f72d9459bb47c
tenet1001 <60763952+tenet1001@users.noreply.github.com>
2020-02-08 16:23:48 -0600

    This is just the Visual Code thingy

---
+++ Rinse and Repeat/Rinse and Repeat/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Rinse_and_Repeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string version = "1.2.0";
+            string title = "Age Guesser";
+            string author = "Tenet";
+            int age;
+            int tries = 0;
+            int maxTries = 3;
+            int guesses = 0;
+            int totalGuesses = 0;
+            Console.WriteLine("Welcome to {0}! Version: {1} Author: {2}", title, version, author);
+            while (true)
+            {
+                Console.WriteLine($"Tries: {tries} | Please input your age: ");
+                
+                var ageString = Console.ReadLine();
+
+                if (Int32.TryParse(ageString, out age))
+                {
+                    if (age <= 100 && age >= 0)
+                    {
+                        Console.WriteLine("Is this right: Y/N: " + age);
+
+                        if (Console.ReadKey().Key == ConsoleKey.Y) 
+                        {
+
+                        }
+                    }
+                    else Console.WriteLine("You are either too old to be alive or too young to be playing this game");
+                }
+
+                if (++tries > maxTries) break;
+            }
+        }
+    }
+}
